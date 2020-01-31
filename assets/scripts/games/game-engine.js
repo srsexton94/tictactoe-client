6009c17be020136760aa999b2e3f6bead1dab849
srsexton94 <srsexton94@gmail.com>
2020-01-31 14:47:15 -0500

    Creates arrays of each players coordinate markers
    Writes code, under gameEngine function, to push the coordinate pairs
    for each chosen spot to separate arrays for each player

--- assets/scripts/games/game-engine.js
+++ assets/scripts/games/game-engine.js
@@ -13,15 +13,31 @@ const gameEngine = response => {
   const gameboard = [...store.user.currentGame.cells]
   // initializes empty array to push chunked subarrays to
   const coordinates = []
-  // chunks gameboard into multidimentional array
+
+  // chunks gameboard into multidimensional array
   // indices represent the coordinate location of cell
   while (gameboard.length > 0) {
     coordinates.push(gameboard.splice(0, gameSize))
   }
 
-  console.log('coordinates: ', coordinates)
-  console.log('gameboard: ', gameboard)
-  console.log('currentGame.Cells: ', store.user.currentGame.cells)
+  const xSpots = []
+  const oSpots = []
+  // pushes the coordinate pairs for each players choice to the initialized arrays above
+  for (let i = 0; i < coordinates.length; i++) {
+    for (let e = 0; e < coordinates[i].length; e++) {
+      const cell = coordinates[i][e]
+      if (cell === 'x') {
+        xSpots.push([i, e])
+      } else if (cell === 'o') {
+        oSpots.push([i, e])
+      }
+    }
+  }
+  // console.log('xSpots: ', xSpots)
+  // console.log('oSpots: ', oSpots)
+  // console.log('coordinates: ', coordinates)
+  // console.log('gameboard: ', gameboard)
+  // console.log('currentGame.Cells: ', store.user.currentGame.cells)
 }
 
 module.exports = {
