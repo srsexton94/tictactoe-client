7c11e259ca11b494c88564471c0b51928e06e164
srsexton94 <srsexton94@gmail.com>
2020-07-23 11:11:34 -0400

    Adjust apiUrl config

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -1,15 +1,15 @@
 'use strict'
 
-const apiUrl = 'https://tictactoe-api-2020.herokuapp.com'
-// const apiUrls = {
-//   production: 'https://tic-tac-toe-wdi-production.herokuapp.com',
-//   development: 'https://tic-tac-toe-wdi.herokuapp.com'
-// }
-// if (window.location.hostname === 'localhost') {
-//   apiUrl = apiUrls.development
-// } else {
-//   apiUrl = apiUrls.production
-// }
+let apiUrl
+const apiUrls = {
+  production: 'https://tictactoe-api-2020.herokuapp.com',
+  development: 'http://localhost:4741'
+}
+if (window.location.hostname === 'localhost') {
+  apiUrl = apiUrls.development
+} else {
+  apiUrl = apiUrls.production
+}
 
 module.exports = {
   apiUrl
